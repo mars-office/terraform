3eefd6e46fbba7493084f7536225fda43a9becab
Alin Florin <alin@qoffice.ro>
2023-04-28 01:19:57 +0300

    -

--- modules/cluster-config/main.tf
+++ modules/cluster-config/main.tf
@@ -12,8 +12,17 @@ terraform {
 }
 
 provider "kubernetes" {
-  host = local.host
+  host                   = local.host
   cluster_ca_certificate = local.ca_cert
-  client_certificate = local.client_cert
-  client_key = local.client_key
+  client_certificate     = local.client_cert
+  client_key             = local.client_key
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = local.host
+    cluster_ca_certificate = local.ca_cert
+    client_certificate     = local.client_cert
+    client_key             = local.client_key
+  }
 }
