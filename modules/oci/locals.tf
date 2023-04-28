c95edadff8b793c9b7de3456aafc7f0c0f16d544
Alin Florin <alin@qoffice.ro>
2023-04-29 02:58:04 +0300

    -

--- modules/oci/locals.tf
+++ modules/oci/locals.tf
@@ -1,5 +1,5 @@
 locals {
   vcn_subnet = "10.0.0.0/16"
   k3s_subnet = "10.0.0.0/24"
-  primaryMasterName = [for v in var.vms : v.name if v.master == true && v.primary == true][0]
+  primaryMasterName = length(var.vms) > 0 ? [for v in var.vms : v.name if v.master == true && v.primary == true][0] : null
 }
