553212d88f52fb17ee0a1e45d819ccace32dd853
Alin Florin <alin@qoffice.ro>
2023-04-29 02:16:37 +0300

    -

--- dev/main.tf
+++ dev/main.tf
@@ -19,17 +19,17 @@ module "oci" {
   sshKeyPublic = var.sshKeyPublic
   env = var.env
   vms = [
-    # {
-    #   name = "k3smaster1"
-    #   shape = "VM.Standard.A1.Flex"
-    #   ram = 24
-    #   cpus = 4
-    #   ssdSize = 200
-    #   osImageId = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa4vff5misacsoq2khjgx5qtqtar6gcga2evkqiioorgllxvgksroq" // Canonical Ubuntu 22.04 minimal aarch64
-    #   availabilityDomain = "UoPG:EU-FRANKFURT-1-AD-1"
-    #   faultDomain = null
-    #   master = true
-    #   primary = true
-    # }
+    {
+      name = "k3smaster1"
+      shape = "VM.Standard.A1.Flex"
+      ram = 24
+      cpus = 4
+      ssdSize = 200
+      osImageId = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa4vff5misacsoq2khjgx5qtqtar6gcga2evkqiioorgllxvgksroq" // Canonical Ubuntu 22.04 minimal aarch64
+      availabilityDomain = "UoPG:EU-FRANKFURT-1-AD-1"
+      faultDomain = null
+      master = true
+      primary = true
+    }
   ]
 }
