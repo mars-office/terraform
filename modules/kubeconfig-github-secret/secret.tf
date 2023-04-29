f23b7730612dfddf6474ceec412c9ff39c4c5171
Alin Florin <alin@qoffice.ro>
2023-04-29 11:42:53 +0300

    -

--- modules/kubeconfig-github-secret/secret.tf
+++ modules/kubeconfig-github-secret/secret.tf
@@ -1,5 +1,5 @@
-# resource "github_actions_organization_secret" "kubeconfig_secret" {
-#   secret_name     = "${upper(var.env)}_KUBECONFIG"
-#   visibility      = "all"
-#   plaintext_value = var.kubeconfig
-# }
+resource "github_actions_organization_secret" "kubeconfig_secret" {
+  secret_name     = "${upper(var.env)}_KUBECONFIG"
+  visibility      = "all"
+  plaintext_value = var.kubeconfig
+}
