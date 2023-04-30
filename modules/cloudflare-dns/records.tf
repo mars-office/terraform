6b31db8e4e09bb8f46a03883b638b253b8960aac
Alin Florin <alin@qoffice.ro>
2023-04-30 13:55:32 +0300

    -

--- modules/cloudflare-dns/records.tf
+++ modules/cloudflare-dns/records.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_record" "record" {
-  for_each = {for r in var.records : join("", [r.name, r.type, r.value]) => r}
+  for_each = {for r in var.records : r => r}
   zone_id = data.cloudflare_zone.zone.id
   name    = each.value.name
   value   = each.value.value
