588521f0ef6f283addf11f768076b41d4b1620ab
Alin Florin <alin@qoffice.ro>
2023-05-02 00:46:54 +0300

    -

--- modules/cluster-config/newrelic.tf
+++ modules/cluster-config/newrelic.tf
@@ -11,21 +11,57 @@ resource "helm_release" "newrelic" {
   values = [<<EOF
 logging:
   enabled: true
+nri-kube-events:
+  enabled: true
 newrelic-logging:
   lowDataMode: true
+  resources:
+    limits:
+      cpu: 500m
+      memory: 128Mi
+    requests:
+      cpu: 1m
+      memory: 1Mi
 global:
   licenseKey: ${var.newRelic.ingestionKey}
   cluster: ${var.env}
   lowDataMode: true
 newrelic-infrastructure:
   privileged: true
+  controlPlane:
+    resources:
+      limits:
+        memory: 300M
+      requests:
+        cpu: 1m
+        memory: 1Mi
+  kubelet:
+    resources:
+      limits:
+        memory: 300M
+      requests:
+        cpu: 1m
+        memory: 1Mi
   ksm:
+    resources:
+      limits:
+        memory: 850M  # Bump me up if KSM pod shows restarts.
+      requests:
+        cpu: 1m
+        memory: 1Mi
     config:
       selector: "app.kubernetes.io/name=kube-state-metrics"
 kube-state-metrics:
   enabled: false
 newrelic-prometheus-agent:
   enabled: false
+nri-metadata-injection:
+  resources:
+    limits:
+      memory: 80M
+    requests:
+      cpu: 1m
+      memory: 1Mi
 EOF
   ]
 
