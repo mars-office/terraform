a55ffcd7b302aa51806aaf2674f417e9a37b7bbc
Alin Florin <alin@qoffice.ro>
2023-05-03 00:11:32 +0300

    -

--- modules/oci/data.tf
+++
@@ -1,3 +0,0 @@
-data "oci_identity_availability_domains" "availability_domains" {
-    compartment_id = var.ociCompartmentOcid
-}
--- prod/main.tf
+++ prod/main.tf
@@ -27,7 +27,7 @@ module "oci" {
       cpus = 4
       ssdSize = 200
       osImageId = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa4vff5misacsoq2khjgx5qtqtar6gcga2evkqiioorgllxvgksroq" // Canonical Ubuntu 22.04 minimal aarch64
-      availabilityDomain = "BzCu:EU-FRANKFURT-1-AD-1"
+      availabilityDomain = "gaep:EU-FRANKFURT-1-AD-1"
       faultDomain = null
       master = true
       primary = true
