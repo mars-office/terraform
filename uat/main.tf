1dee8da908515c31eacbbac0daeec61c8200239e
Alin Florin <alin@qoffice.ro>
2023-05-02 23:23:21 +0300

    -

--- uat/main.tf
+++ uat/main.tf
@@ -40,6 +40,7 @@ module "ssh-extract-kubeconfig" {
   source = "../modules/ssh-extract-kubeconfig"
   sshKeyPrivate = var.sshKeyPrivate
   ip = [for vm in module.oci.vms : vm.public_ip if vm.primary == true][0]
+  clusterDns = "${var.env}.marsconceptor.com"
 }
 
 module "kubeconfig-github-secret" {
@@ -47,7 +48,6 @@ module "kubeconfig-github-secret" {
   ghToken = var.ghToken
   kubeconfig = module.ssh-extract-kubeconfig.kubeconfig_with_hostname
   env = var.env
-  clusterDns = "${var.env}.marsconceptor.com"
 }
 
 module "cloudflare-dns-cluster" {
