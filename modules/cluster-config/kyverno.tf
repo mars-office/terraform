a749328b78a24e90b8cea1cda09cd80e55dc03be
Alin Florin <alin@qoffice.ro>
2023-05-16 02:05:37 +0300

    -

--- modules/cluster-config/kyverno.tf
+++ modules/cluster-config/kyverno.tf
@@ -39,3 +39,64 @@ resource "kubernetes_cluster_role_binding" "kyverno-cluster-admin-role-binding" 
     helm_release.kyverno[0]
   ]
 }
+
+resource "kubernetes_cluster_role_binding" "kyverno-cluster-admin-role-binding-2" {
+  metadata {
+    name = "kyverno-cluster-admin-role-binding-2"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+  subject {
+    kind = "ServiceAccount"
+    namespace = "kyverno"
+    name = "kyverno-admission-controller"
+  }
+  count = var.kyverno.enabled ? 1 : 0
+  depends_on = [
+    helm_release.kyverno[0]
+  ]
+}
+
+resource "kubernetes_cluster_role_binding" "kyverno-cluster-admin-role-binding-3" {
+  metadata {
+    name = "kyverno-cluster-admin-role-binding-3"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+  subject {
+    kind = "ServiceAccount"
+    namespace = "kyverno"
+    name = "kyverno-reports-controller"
+  }
+  count = var.kyverno.enabled ? 1 : 0
+  depends_on = [
+    helm_release.kyverno[0]
+  ]
+}
+
+resource "kubernetes_cluster_role_binding" "kyverno-cluster-admin-role-binding-4" {
+  metadata {
+    name = "kyverno-cluster-admin-role-binding-4"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+  subject {
+    kind = "ServiceAccount"
+    namespace = "kyverno"
+    name = "kyverno-cleanup-controller"
+  }
+  count = var.kyverno.enabled ? 1 : 0
+  depends_on = [
+    helm_release.kyverno[0]
+  ]
+}
+
