a0f56976d447173968f141f27b18d57c2c9ec54e
Alin Florin <alin@qoffice.ro>
2023-06-03 03:42:47 +0300

    asd

--- local/main.tf
+++ local/main.tf
@@ -55,7 +55,7 @@ module "cluster-config" {
   vdi = {
     enabled = false
     githubToken = var.ghToken
-    version = "0.0.49"
+    version = "0.0.50"
     vdis = [
       {
         name = "test"
