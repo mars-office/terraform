8054d551a712a3891713942bac658607a90b3310
Alin Florin <alin@qoffice.ro>
2023-06-05 22:00:50 +0300

    -

--- modules/cluster-config/telepresence.tf
+++ modules/cluster-config/telepresence.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "telepresence" {
-  name       = "telepresence"
+  name       = "traffic-manager"
   repository = "https://app.getambassador.io"
-  chart      = "traffic-manager"
-  version    = "2.10.2"
+  chart      = "telepresence"
+  version    = "2.13.3"
   create_namespace = true
   namespace = "ambassador"
   timeout = 1500
