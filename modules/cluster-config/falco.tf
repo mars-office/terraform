c5da4fb2aee0b43bd38a7b4ff8e4c83f1e6b6c7d
Alin Florin <alin@qoffice.ro>
2023-10-14 18:46:37 +0300

    -

--- modules/cluster-config/falco.tf
+++ modules/cluster-config/falco.tf
@@ -1,26 +1,3 @@
-resource "helm_release" "falco-basic-auth-secret" {
-  name             = "falco-basic-auth-secret"
-  repository       = "https://helm.sikalabs.io"
-  chart            = "basic-auth-secret"
-  version          = "1.0.0"
-  create_namespace = true
-  namespace        = "falco"
-  timeout          = 500
-  wait = true
-  values = [<<EOF
-user: 'admin'
-password: '${var.falco.adminPassword}'
-EOF
-  ]
-
-  count = var.falco.enabled ? 1 : 0
-
-
-  depends_on = [
-    helm_release.linkerd-control-plane[0]
-  ]
-}
-
 resource "helm_release" "falco" {
   name       = "falco"
   repository = "https://falcosecurity.github.io/charts"
@@ -39,39 +16,57 @@ resources:
   requests:
     cpu: 1m
     memory: 1Mi
-falcosidekick:
-  enabled: true
-  podAnnotations:
-    linkerd.io/inject: enabled
-  replicaCount: 1
-  webui:
-    enabled: true
-    podAnnotations:
-      linkerd.io/inject: enabled
-    redis:
-      podAnnotations:
-        linkerd.io/inject: enabled
-    replicaCount: 1
-    loglevel: info
-    disableauth: true
-    ingress:
-      enabled: true
-      annotations:
-        kubernetes.io/ingress.class: nginx
-        nginx.ingress.kubernetes.io/ssl-redirect: "true"
-        cert-manager.io/cluster-issuer: ${var.certManager.issuer}
-      hosts:
-        - host: falco.${var.clusterDns}
-          paths:
-            - path: /
-      tls:
-        - secretName: falco-ingress-tls
-          hosts:
-            - falco.${var.clusterDns}
 EOF
   ]
 
   count = var.falco.enabled ? 1 : 0
 
-  depends_on = [ helm_release.falco-basic-auth-secret[0]]
+  depends_on = [ helm_release.linkerd-control-plane[0]]
+}
+
+resource "helm_release" "falcosidekick" {
+  name       = "falcosidekick"
+  repository = "https://falcosecurity.github.io/charts"
+  chart      = "falcosidekick"
+  version    = "0.7.2"
+  create_namespace = true
+  namespace = "falco"
+  timeout = 1500
+  wait = true
+
+  values = [<<EOF
+podAnnotations:
+  linkerd.io/inject: enabled
+replicaCount: 1
+webui:
+  enabled: true
+  podAnnotations:
+    linkerd.io/inject: enabled
+  redis:
+    podAnnotations:
+      linkerd.io/inject: enabled
+  replicaCount: 1
+  loglevel: info
+  disableauth: false
+  user: admin:${var.falco.adminPassword}
+  ingress:
+    enabled: true
+    annotations:
+      kubernetes.io/ingress.class: nginx
+      nginx.ingress.kubernetes.io/ssl-redirect: "true"
+      cert-manager.io/cluster-issuer: ${var.certManager.issuer}
+    hosts:
+      - host: falco.${var.clusterDns}
+        paths:
+          - path: /
+    tls:
+      - secretName: falco-ingress-tls
+        hosts:
+          - falco.${var.clusterDns}
+EOF
+  ]
+
+  count = var.falco.enabled ? 1 : 0
+
+  depends_on = [ helm_release.falco[0]]
 }
