aaab32db7f4904eec0d498245c7c6ba1bb60899d
Alin Florin <alin@qoffice.ro>
2024-01-01 21:48:48 +0200

    -

--- modules/oci/main.tf
+++ modules/oci/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "5.20.0"
+      version = "5.23.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.5.1"
+      version = "3.6.0"
     }
     cloudinit = {
       source = "hashicorp/cloudinit"
-      version = "2.3.2"
+      version = "2.3.3"
     }
   }
 }
